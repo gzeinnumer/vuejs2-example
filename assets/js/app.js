16f47bfc7ea070f6be4b061451e3b8f6590a574d
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-02 22:15:43 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
+//cuma render text
 var app = new Vue({
     el: "#app",
     data : {
-        msg : "Hello Zein"
+        msg : "<i>Hello Zein</i>"
+    }
+})
+
+//render dengan tag htmlnya yang tereksekusi
+var app2 = new Vue({
+    el: "#app2",
+    data : {
+        msg : "<i>Hello Zein</i>"
+    }
+})
+
+//render dengan tag htmlnya yang diabaikan
+var app3 = new Vue({
+    el: "#app2",
+    data : {
+        msg : "<i>Hello Zein</i>"
     }
 })
