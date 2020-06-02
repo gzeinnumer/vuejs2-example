188a5cb9c710b354d7bb11ed1f2381d081cf1d40
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-02 22:28:58 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 var app = new Vue({
     el: "#app",
     data : {
-        msg : "Hello Zein"
+        msg : "Hello Zein",
+        msg2 : "Hello GZeinNumer",
+        show : true,
+        isReady : 'ready'
     }
 })
