5c1f4a19d6bc4c8853e03b55b16b4d655b763fb1
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-03 21:14:48 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,8 @@
 var app = new Vue({
     el: "#app",
     data : {
-        msg : "Hello Zein -> watch untuk memantau sebuah inputan text, jika di masukan value, maka akan memanggil watch dan mengeksekusi function sesuai nama variable",
-        km : 0,
-        m : 0
+        msg : "Hello Zein -> Directive v-model seperti MVVM di android",
+        message : ""
     }, 
-    watch: {
-        km : function(p1){
-            this.km = p1
-            this.m = p1 * 1000
-        },
-        m : function(p1){
-            this.km = p1/1000
-            this.m = p1
-        }
-    },
 
 })
