89d604009009d20c9126caa37be94408cf884a6f
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-03 20:55:13 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
 var app = new Vue({
     el: "#app",
     data : {
-        msg : "Hello Zein"
-    }
+        msg : "Hello Zein",
+        int1 : 1,
+        int2 : 2
+    }, 
+    computed: {
+        sum: function(){
+            return this.int1 + this.int2            
+        },
+    },
 })
