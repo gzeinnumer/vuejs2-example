2e5086de29c0af093f1b9dc97a9fe1cee85a436e
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-06 22:50:58 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,23 @@
+Vue.component('greeting', {
+    template : "<b>Hay Zein, by component</b>"
+});
+
+Vue.component('greeting2', {
+    template : `
+        <div>
+            <b>Hello Zein</b> <br>
+            <i>Hello Zein</i> <br>
+            <u>Hello Zein</u>
+        </div>
+    `
+});
+//petik dekat angka 1 pada keyboard
+
 var app = new Vue({
     el: "#app",
     data : {
-        msg : "Hello Zein -> Object ",
-        vehicles : [
-            {type: "Car1", brand : "Toyota1", color : "Metallic1"},
-            {type: "Car2", brand : "Toyota2", color : "Metallic2"},
-            {type: "Car3", brand : "Toyota3", color : "Metallic3"},
-        ],
-        object : {
-            fname : "M Fadli",
-            lname :  "Zein",
-            age : 23
-        }
+        msg : "Hello Zein -> Component ",
     }, 
-})
+});
 
 //cari tau kode as key
