84580140e26d40ee0b9d970c83c518149c238316
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-06 20:21:25 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
 var app = new Vue({
     el: "#app",
     data : {
-        msg : "Hello Zein -> Directive v-model dropdown",
-        select : '',
-        selectMutli : []
+        msg : "Hello Zein -> Directive v-bind",
+        imgFile : "assets/img/logo.png",
+        link : "google.com",
+        colorFont : "green",
+        styleFont : "color : red; font-size: 20px;"
     }, 
 
 })
