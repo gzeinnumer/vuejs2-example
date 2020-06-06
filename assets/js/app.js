149a0c6750e4654210ffd1cd3fe8e3280e837a54
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-06 22:39:09 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,18 @@
 var app = new Vue({
     el: "#app",
     data : {
-        msg : "Hello Zein -> Directive v-on",
-        content : "",
-        vehicles : ["mobil","motor","sepeda"]
-    }, 
-    methods: {
-        addVehicles: function(){
-            this.vehicles.push(this.content);
-            this.content = "";
-        },
-        removeVehicle : function(index){
-            this.vehicles.splice(index, 1);
+        msg : "Hello Zein -> Object ",
+        vehicles : [
+            {type: "Car1", brand : "Toyota1", color : "Metallic1"},
+            {type: "Car2", brand : "Toyota2", color : "Metallic2"},
+            {type: "Car3", brand : "Toyota3", color : "Metallic3"},
+        ],
+        object : {
+            fname : "M Fadli",
+            lname :  "Zein",
+            age : 23
         }
-    },
+    }, 
 })
 
 //cari tau kode as key
