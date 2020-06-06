a32e5a701ba974a1ac832b1596428e3143225cf2
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-06 23:04:07 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,21 @@
-
-
-Vue.component('button-center', {
-    data : function(){
-        return{
-            count : 0
-        }
-    },
-    template : "<button v-on:click='count++'>Clicked Me {{ count }} Times</button>"
+Vue.component('blog-post', {
+    props : ['post'],
+    template : `
+        <div> 
+            <h3>{{ post.title }}</h3>
+            <div v-html="post.content"></div>
+        </div>
+    `
 });
 
 var app = new Vue({
     el: "#app",
     data : {
         msg : "Hello Zein -> Component ",
+        posts_ : [
+            { id: 1, title: "GZeinNumer1", content:"M. Fadli Zein1"},
+            { id: 2, title: "GZeinNumer2", content:"M. Fadli Zein2"},
+            { id: 3, title: "GZeinNumer3", content:"M. Fadli Zein3"},
+        ]
     }, 
 });
