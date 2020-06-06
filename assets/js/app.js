f9d8b7febf69d9b4a0026cea3fc61511514a51df
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-06 22:14:40 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,22 @@ var app = new Vue({
         message : ""
     }, 
     methods: {
-        showAlert: function(){
-            alert('Alert!');
-        }
+        escKey : function(){
+            this.message = "escKey"
+        },
+        spaceKey : function(){
+            this.message = "spaceKey"
+        },
+        upKey : function(){
+            this.message = "upKey"
+        },
+        downKey : function(){
+            this.message = "downKey"
+        },
+        aKey : function(){
+            this.message = "'a' Key"
+        },
     },
-
 })
+
+//cari tau kode as key
