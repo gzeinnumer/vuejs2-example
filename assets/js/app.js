c031c1bcbb513983fce63b19980330c69fce7692
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-06-06 20:33:43 +0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 var app = new Vue({
     el: "#app",
     data : {
-        msg : "Hello Zein -> Directive v-bind",
-        imgFile : "assets/img/logo.png",
-        link : "google.com",
-        colorFont : "green",
-        styleFont : "color : red; font-size: 20px;"
+        msg : "Hello Zein -> Directive v-on",
+        message : ""
     }, 
+    methods: {
+        showAlert: function(){
+            alert('Alert!');
+        }
+    },
 
 })
